043b1827c488506f96e6e391ed48892f67f23969
Dan Dowd <danieldowd89@gmail.com>
2023-03-25 12:48:44 -0600

    removed unused methods

--- ApplicationService/SubmissionRepository.cs
+++ ApplicationService/SubmissionRepository.cs
@@ -7,16 +7,6 @@ public class SubmissionRepository : IRepository<SubmissionEntity>
         return Task.CompletedTask;
     }
 
-    public Task Delete(SubmissionEntity entity)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<IEnumerable<SubmissionEntity>> GetAll()
-    {
-        throw new NotImplementedException();
-    }
-
     public Task<SubmissionEntity> GetById(string id)
     {
         throw new NotImplementedException();
